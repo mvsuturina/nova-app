af10d71f1ee04a2a60d43e04399fac37d878bb9c
Marina Suturina <mvsuturina@avito.ru>
2026-06-16 22:41:05 +0300

    update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nova-app-v35';
+const CACHE = 'nova-app-v36';
 
 // Install: activate immediately without waiting for old tabs to close
 self.addEventListener('install', () => self.skipWaiting());
